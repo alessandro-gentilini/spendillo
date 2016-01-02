ac025425f6c0cc6c76ca273db1d76ee917478ede
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-01-02 18:28:41 +0100

    Add date time conversion.

--- spendillo.R
+++ spendillo.R
@@ -1,5 +1,14 @@
-df <- read.csv('test_data.txt',sep=',',head=TRUE)
+df <- read.csv('data.txt',sep=';',head=TRUE,dec=',')
+
+# remove last column
+df$X <- NULL
+
+# convert date time
+df$date_time <- as.POSIXct(strptime(df$date_time,'%d/%m/%Y %H:%M:%S'))
+
+# compute sub totals
 sub_totals <- aggregate(cbind(value)~category,data=df,FUN = sum)
-print(sub_totals)
+
 print(sub_totals[order(sub_totals$category),])
 print(sub_totals[order(-sub_totals$value),])
+
